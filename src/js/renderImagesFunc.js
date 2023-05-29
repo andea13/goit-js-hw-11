6f6fc4d8d1b5cfb70262799afb040bb2a8ebdd9f
Oksana Dziuba <dziuba131303@gmail.com>
2023-05-29 13:08:14 +0400

    another correction of console error

--- src/js/renderImagesFunc.js
+++ src/js/renderImagesFunc.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import refs from './refs';
 import onFormSubmit from './index';
 
